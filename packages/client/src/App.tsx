db6e42ad48a0ccb3554c9a578be72a60a521d731
Petr Gusev <dack2702@rambler.ru>
2022-10-09 18:20:35 +0300

    react-router-dom

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,31 @@
-import { useEffect } from 'react'
+import { CssBaseline } from '@material-ui/core';
+import { Routes, Route} from 'react-router-dom';
 import './App.css'
+import GamePage from './pages/GamePage';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import { BASE_URL, GAME_URL, LOGIN_URL, SIGNUP_URL } from './utils/constants';
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
 
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  //   fetchServerData()
+  // }, [])
+  return <>
+    <CssBaseline />
+    <Routes>
+      <Route path={BASE_URL} element={<Login />} />
+      <Route path={LOGIN_URL} element={<Login />} />
+      <Route path={SIGNUP_URL} element={<SignUp />} />
+      <Route path={GAME_URL} element={<GamePage />} />
+    </Routes>
+  </>
 }
 
-export default App
+export default App;
---
+++ packages/client/src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = '/';
+export const LOGIN_URL = '/login';
+export const SIGNUP_URL = '/signup';
+export const GAME_URL = '/game';
