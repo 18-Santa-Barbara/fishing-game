6e4f730caece8d170a627d447813ed031191cfdd
Sergey Kenash <krisvis31@mail.ru>
2022-11-18 11:12:16 +0300

    create test

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,11 +1,15 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
-}
+  moduleNameMapper: {
+    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy',
+  },
+};
