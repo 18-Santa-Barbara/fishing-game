68ebde12eb3ca2cd758d58f13344ed7df784cf2f
Petr Gusev <dack2702@rambler.ru>
2022-12-19 16:57:41 +0300

    conflicts

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,28 +2,13 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
 import { skipToken } from '@reduxjs/toolkit/dist/query';
 import './App.css';
-import ProtectedRoute from './hocs/protected-route/ProtectedRoute';
-import ChangePassPage from './pages/ChangePassPage';
-import Forum from './pages/Forum';
-import Login from './pages/Login';
-import ProfilePage from './pages/ProfilePage';
-import SignUp from './pages/SignUp';
-import { Leaderboard } from './pages/Leaderboard';
-import ErrorBoundary from './pages/components/ErrorBoundary';
-import Comments from './pages/Comments';
-import {
-  BASE_URL,
-  CHANGE_PASS_URL,
-  GAME_URL,
-  LOGIN_URL,
-  PROFILE_URL,
-  FORUM_URL,
-  SIGNUP_URL,
-  LEADERBOARD_URL,
-  COMMENTS_URL,
-} from './utils/constants';
-import { NoSsr } from '@mui/material';
-import GamePage from './pages/GamePage';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import RootRouter from './components/RootRouter';
+import StartSpinner from './components/StartSpinner';
+import { useGetThemeQuery } from './services/themeApi';
+import { useGetUserQuery } from './services/userApi';
+import { darkTheme, lightTheme } from './utils/theme';
 
 function App() {
   const { isSuccess, data, isFetching, isError } = useGetUserQuery();
