88c1a37b46af9b69bd08d3ff1af49b455125dad5
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-12-20 21:03:44 +0300

    port

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -11,4 +11,4 @@ export const COMMENTS_URL = '/comments'
 export const API = 'https://ya-praktikum.tech/api/v2';
 export const REDIRECT_URI = 'http://santa-barbara-18.ya-praktikum.tech';
 
-export const PORT = 'http://localhost:8080/api';
+export const PORT = 'http://localhost:3001/api';
